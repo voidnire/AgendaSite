24fcff38766e37b6985f3f9ba4ecac4b00e82c6d
voidnire <nirerin15@gmail.com>
2022-05-16 09:53:44 -0400

    project

---
+++ BlazorApp4/Data/AgendaDBContext.cs
@@ -0,0 +1,16 @@
+﻿using BlazorApp4.Data.Entities;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp4.Data
+{
+    public class AgendaDBContext : DbContext
+    {
+        public AgendaDBContext(DbContextOptions<AgendaDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Todo> {get; set;}
+    }
+}
---
+++ BlazorApp4/Data/Entities/Todo.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp4.Data.Entities
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Description { get; set; }
+        public string Priority { get; set; }
+        public string Category { get; set; }
+
+    }
+}
